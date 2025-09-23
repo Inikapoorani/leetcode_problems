7d4d146147145160c77eed504b066a5a8c92719c
INIKA P S <155460587+Inikapoorani@users.noreply.github.com>
2025-09-23 13:33:13 +0530

    Time: 17 ms (86.56%), Space: 95 MB (17.67%) - LeetHub

---
+++ 0077-combinations/0077-combinations.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void help(int cur,List<List<Integer>> result,ArrayList<Integer>temp,int k,int n){
+        if(temp.size()==k){
+            result.add(new ArrayList<>(temp));
+            return;
+        }
+        if(cur>n) return;
+        temp.add(cur);
+        help(cur+1,result,temp,k,n);
+        temp.remove(temp.size()-1);
+        help(cur+1,result,temp,k,n);
+    }
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> result = new ArrayList<>(); 
+        help(1,result,new ArrayList<>(),k,n);
+        return result;
+    }
+}
