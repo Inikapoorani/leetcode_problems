4d88e223e1ec493d3ebbce0c08816009e8cddda8
INIKA P S <155460587+Inikapoorani@users.noreply.github.com>
2025-09-24 11:48:38 +0530

    Time: 2 ms (83.98%), Space: 44.7 MB (64.5%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,62 +1,38 @@
  class Solution {
-    private void backtrack(int[] candidates, int idx, int target, List<Integer> cur, int sum, List<List<Integer>>result) {
-        if (sum == target) {
-            result.add(new ArrayList<>(cur));
-            return;
-        }
-        if (idx >= candidates.length || sum > target) return;
-        cur.add(candidates[idx]);
-        backtrack(candidates, idx, target, cur, sum + candidates[idx],result);
-        cur.remove(cur.size() - 1); 
-        backtrack(candidates, idx + 1, target, cur, sum,result);
-    }
+    // private void backtrack(int[] candidates, int idx, int target, List<Integer> cur, int sum, List<List<Integer>>result) {
+    //     if (sum == target) {
+    //         result.add(new ArrayList<>(cur));
+    //         return;
+    //     }
+    //     if (idx >= candidates.length || sum > target) return;
+    //     cur.add(candidates[idx]);
+    //     backtrack(candidates, idx, target, cur, sum + candidates[idx],result);
+    //     cur.remove(cur.size() - 1); 
+    //     backtrack(candidates, idx + 1, target, cur, sum,result);
+    // }
+    // public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    //     List<List<Integer>> result = new ArrayList<>();
+    //     backtrack(candidates, 0, target, new ArrayList<>(), 0,result);
+    //     return result;
+    // }
+    // }
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> result = new ArrayList<>();
-        backtrack(candidates, 0, target, new ArrayList<>(), 0,result);
+        Arrays.sort(candidates);
+        backtrack(candidates, 0, target, new ArrayList<>(), result);
         return result;
     }
+    public void backtrack(int[] candidates, int idx, int target, List<Integer> cur,List<List<Integer>>result) {
+        if (target==0) {
+             result.add(new ArrayList<>(cur));
+             return;
+        }
+        for(int i=idx;i<candidates.length;i++){
+            if(candidates[i]>target) break;
+            cur.add(candidates[i]);
+            backtrack(candidates,i,target-candidates[i],cur,result);
+            cur.remove(cur.size()-1);
+        }
     }
-
-    // public void combinationSum(int[] candidates, int target){
-    //     List<Integer> temp;
-    //     List<List<Integer>> result = new ArrayList<>();
-    //     int sum=0;
-    //     int n=candidates.length;
-    //     for(int i=0;i<n;i++){
-    //         sum=candidates[i];
-    //         for(int j=0;j<n;j++){
-    //             if(sum==target){
-    //                 temp.add(candidates[i]);
-    //                 result.add(new ArrayList<>(temp));
-    //                 break;
-    //             }
-    //             else if(sum!=target){
-    //                 sum+=candidates[j];
-    //                 if(sum==target){
-    //                  temp.add(candidates[i]);
-    //                  result.add(new ArrayList<>(temp));
-    //                  break;
-    //                 }
-    //                 if(sum>target){
-    //                     break;
-    //                 }
-    //             }
-    //         }
-    //     }
-    //     return result;
-       
-    //     if(target < 0) return;
-    //     if(target == sum){
-    //         result.add(new ArrayList<>(temp));
-    //         return;
-    //     }    
-    //     if(index >= candidates.length) return;
-    //     temp.add(candidates[index]);
-    //     combinationSum(candidates,result,index+1,temp);
-    //     temp.remove(temp.size()-1);
-    //     combinationSum(candidates,result,index+1,temp);
-    // }
-    // public List<List<Integer>> combinationSum(int[] candidates,int result, int target) {
-    //     List<List<Integer>> result = new ArrayList<>();
-    //     getCombinations(result, candidates, target, 0, new ArrayList<>());
-    //     return result;
+ }
+    
