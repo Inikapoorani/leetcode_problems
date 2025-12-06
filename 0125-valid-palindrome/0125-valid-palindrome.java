78acd9d213abb44fda88e9c922d4e21b0d7a0c3a
INIKA P S <155460587+Inikapoorani@users.noreply.github.com>
2025-12-06 14:19:11 +0530

    Time: 4 ms (51.99%), Space: 44.7 MB (45.25%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -6,6 +6,15 @@ class Solution {
                 str.append(Character.toLowerCase(ch));
             }
         }
-           return str.toString().equals(str.reverse().toString());
+           return cp(str.toString()); //str.toString().equals(str.reverse().toString());
     }
+  private boolean cp(String r){
+    int i=0,j=r.length()-1;
+    while(i<j){
+        if(r.charAt(i)!=r.charAt(j))return false;
+        i++;
+        j--; 
+    }
+    return true;
+}
 }
