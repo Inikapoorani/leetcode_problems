afce40c8bb025605cfc9cb16f14974e196418c7a
INIKA P S <155460587+Inikapoorani@users.noreply.github.com>
2026-05-17 23:16:49 +0530

    Time: 3 ms (87.75%), Space: 42.8 MB (97.85%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,31 +1,32 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-
-        
-            if (c == '(' || c == '{' || c == '[') {
-                stack.push(c);
-            } 
-            
-            else {
-                if (stack.isEmpty()) {
-                    return false;
-                }
-
-                char top = stack.pop();
-
-                if (c == ')' && top != '(') 
-                {
-                    return false;
-                }
-                if (c == '}' && top != '{') return false;
-                if (c == ']' && top != '[') return false;
+        Stack<Character> stack=new Stack<>();
+        char top;
+        for(char ch:s.toCharArray()){
+            if(ch=='('||ch=='['||ch=='{'){
+                stack.push(ch);
             }
-        }
-
-        return stack.isEmpty();
+            else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+                top=stack.pop();
+               if (ch == ')' && top != '(') {
+                    return false;
+                }
+                else if (ch == '}' && top != '{') {
+                    return false;
+                }
+                else if (ch == ']' && top != '[') {
+                    return false;
+                    }
+                }
+            }
+    if(stack.isEmpty()){
+        return true;
+    }
+    else{
+        return false;
+    }
     }
 }
